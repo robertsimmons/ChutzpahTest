8fb2b13d05431a428a90ec5ba443c1ac4ef9b0d7
Robert Simmons <robert.simmons.tn@gmail.com>
2015-09-05 14:34:22 -0500

    Sample project of Chutzpah 4.1.0 not working. Works fine in 4.0.3

---
+++ ChutzpahTest/MySpecs.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="derp.js"/>
+describe('SomeThing', function () {
+
+	describe('when blah', function () {
+		it('foo', function () {
+			expect(foo).toEqual("widget");
+		});
+	});
+
+});
